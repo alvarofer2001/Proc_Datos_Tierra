de78ab6d1da8d805a4d11b46ee1cf44b25e249b5
alvarofer2001 <alvafergo@gmail.com>
2025-10-01 11:19:23 +0200

    función freq2d
    Hacer la funcion freq2d que convertirá las frecuencias a 2D

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -91,6 +91,32 @@ class mtf:
         :return fnAct: 1D normalised frequencies 2D ACT (f/(1/w))
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
+        # DONE
+        fstepAlt = 1 / nlines / w
+        fstepAct = 1 / ncolumns / w
+
+        # 1D frequency vectors
+        eps = 1e-10
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        # Normalize the frequencies with the cut-off
+        fc = D / (lambd * focal)
+
+        frAct = fAct / fc
+        frAlt = fAlt / fc
+
+        fnAct = fAct / (1 / w)
+        fnAlt = fAlt / (1 / w)
+
+        # 2D frequency grids
+        [fnAltxx, fnActxx] = np.meshgrid(fnAlt, fnAct,
+                                         indexing='ij')  # Please use ‘ij’ indexing or you will get the transpose
+        fn2D = np.sqrt(fnAltxx * fnAltxx + fnActxx * fnActxx)
+
+        [frAltxx, frActxx] = np.meshgrid(frAlt, frAct,
+                                         indexing='ij')  # Please use ‘ij’ indexing or you will get the transpose
+        fr2D = np.sqrt(frAltxx * frAltxx + frActxx * frActxx)
         #TODO
         return fn2D, fr2D, fnAct, fnAlt
 
