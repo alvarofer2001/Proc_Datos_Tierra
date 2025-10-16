d1f6ac67af3d0f008113f2b89c4d6ca9ed7021d1
alvarofer2001 <alvafergo@gmail.com>
2025-10-16 18:44:06 +0200

    cambio directorios mainISM

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,7 +5,7 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\TD\PROYECTO\\Proc_Datos_Tierra\\auxiliary"
-indir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150"
+indir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-E2E\\sgm_out"
 outdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\myoutput_ism"
 
 
