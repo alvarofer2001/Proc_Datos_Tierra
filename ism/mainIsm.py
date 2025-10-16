0603f4aa7cc0208d04e86cf7a57d27f95fc283e3
alvarofer2001 <alvafergo@gmail.com>
2025-10-16 18:32:26 +0200

    creacion myoutput_ism

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -6,7 +6,7 @@ from ism.src.ism import ism
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\TD\PROYECTO\\Proc_Datos_Tierra\\auxiliary"
 indir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150"
-outdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\myoutput"
+outdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\myoutput_ism"
 
 
 # Initialise the ISM
