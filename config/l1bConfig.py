14b95f638eb1570fdb2442f4afbbfa6c38dda727
alvarofer2001 <alvafergo@gmail.com>
2025-10-16 18:52:12 +0200

    l1bConfig
    No había puesto self.

--- config/l1bConfig.py
+++ config/l1bConfig.py
@@ -14,7 +14,7 @@ class l1bConfig:
         #--------------------------------------------------------------------------------
         # Gain, conversion factor from Digital Numbers to Radiances
         #self.gain = np.array([0.09209303, 0.06787323, 0.052162305, 0.047756273]) # [mW/m2/sr/DN]
-        gain = np.array([0.005764054, 0.0042465106, 0.0032643573, 0.002987937])  # [mW/m2/sr/DN]
+        self.gain = np.array([0.005764054, 0.0042465106, 0.0032643573, 0.002987937])  # [mW/m2/sr/DN]
 
         # Equalisation, multiplicative and additive factors.
         self.eq_mult = 'equalization/eq_mult_'
