be9cc9df103929022c1dcdecd0901892ab1336e4
alvarofer2001 <alvafergo@gmail.com>
2025-10-23 19:30:35 +0200

    cambio directorios

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -8,6 +8,8 @@ auxdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\TD\PROYECTO\\Proc_Da
 indir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-E2E\\sgm_out"
 outdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-E2E\\myoutput_ism"
 
+#indir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150"
+#outdir = r"C:\\Users\\alvaf\\OneDrive\\Desktop\\Carlos III\\Cuatri III\\Proc_datos_espacio\\EODP_TER_2021\\EODP-TS-ISM\\myoutput"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
